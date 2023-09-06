e8926299517b2d35568f6d34738befd3c01b71aa
Bernardo Margulis <bernardo@thismakesmehappy.co>
2023-09-06 08:01:31 -0400

    Add functions

--- functions.php
+++ functions.php
@@ -68,3 +68,13 @@ function prevent_deleting_pTags($init){
 
     return $init;
 }
+
+// Enable shortcodes
+add_filter( 'widget_text', 'shortcode_unautop');
+add_filter( 'widget_text', 'do_shortcode');
+add_filter( 'the_excerpt', 'shortcode_unautop');
+add_filter( 'the_excerpt', 'do_shortcode');
+add_filter( 'term_description', 'shortcode_unautop');
+add_filter( 'term_description', 'do_shortcode' );
+add_filter( 'comment_text', 'shortcode_unautop');
+add_filter( 'comment_text', 'do_shortcode' );

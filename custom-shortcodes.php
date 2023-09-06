6468a9223d3d6aba628ebd305877968a1485d88b
Bernardo Margulis <bernardo@thismakesmehappy.co>
2023-09-06 18:46:26 -0400

    Update grid

--- custom-shortcodes.php
+++ custom-shortcodes.php
@@ -31,7 +31,82 @@ function return_featured_url()
     return "'" . get_the_post_thumbnail_url() . "'";
 }
 
+
+function post_grid($atts = [], $content = null, $tag = ''): string
+{
+    $gradient = $atts['gradient'];
+    $projects_string = $atts['projects'];
+    $projects = explode(",", $projects_string);
+    $parent = $atts['parent'];
+    $col = $atts['col'];
+    $col_sm = $atts['col_sm'];
+    $col_md = $atts['col_md'];
+    $col_lg = $atts['col_lg'];
+    $col_xl = $atts['col_xl'];
+    $border = $atts['border'];
+    $corner = $atts['corner'];
+
+    if (!isset($col)) {
+        $col = '12';
+    }
+    if (!isset($col_sm)) {
+        $col_sm = $col;
+    }
+    if (!isset($col_md)) {
+        $col_md = $col_sm;
+    }
+    if (!isset($col_lg)) {
+        $col_lg = $col_md;
+    }
+    if (!isset($col_xl)) {
+        $col_xl = $col_lg;
+    }
+    if (!isset($border)) {
+        $border = '1';
+    }
+    if (!isset($corner)) {
+        $corner = '0';
+    }
+
+
+    $item_to_return = "";
+
+
+    $item_to_return .= " <div class = \"row gx-md-2\" data-masonry='{\"percentPosition\": true }'>";
+    foreach ($projects as $project) {
+        if (isset($parent)) {
+            $id = get_page_by_path("{$parent}/{$project}")->ID;
+        } else {
+            $id = get_page_by_path("{$project}")->ID;
+        }
+
+        $brief = get_post_meta($id, "brief", true);
+        $permalink = get_permalink($id);
+        $post_thumbnail_url = get_the_post_thumbnail_url($id, 'thumbnail-grid');
+        $title = get_the_title($id);
+
+        $item_to_return .= "<div class=\" col col-$col col-sm-$col_sm col-md-$col_md col-lg-$col_lg col-xl-$col_xl border-2 rounded-0 p-2\">";
+        $item_to_return .= "<div class=\"card border-$border \">";
+        $item_to_return .= "<a href = \"{$permalink}\" class=\"text-decoration-none\">";
+
+        $item_to_return .= "<img src=\"{$post_thumbnail_url}\" class=\"card-img-top img-fluid\" alt=\"...\">";
+        $item_to_return .= "<div class=\"card-body\">";
+        $item_to_return .= " <h3 class=\"card-title mb-0\"> {$title} </h3>";
+        $item_to_return .= "<p class=\"card-text\"> {$brief} </p>";
+        $item_to_return .= "<span  class=\"btn btn-primary $gradient border-0\">See more</span>";
+        $item_to_return .= "</a>";
+        $item_to_return .= "</div>";
+        $item_to_return .= "</div>";
+        $item_to_return .= "</div>";
+    }
+    $item_to_return .= "</div>";
+
+    return $item_to_return;
+
+}
+
 add_shortcode('happy_upload_dir', 'uploads_folder');
 add_shortcode('happy_media_path', 'media_path');
 add_shortcode('happy_featured_url', 'return_featured_url');
 add_shortcode('happy_url_from_id', 'url_from_id');
+add_shortcode('happy_post_grid', 'post_grid');
--- functions.php
+++ functions.php
@@ -13,14 +13,15 @@ function my_custom_theme_sidebar()
         'id' => 'sidebar-1',
     ));
 }
+
 add_action('widgets_init', 'my_custom_theme_sidebar');
 
 // Suport featured image
 add_theme_support('post-thumbnails');
 
 // Support excerpt
-add_post_type_support( 'page', 'excerpt' );
-add_post_type_support( 'post', 'excerpt' );
+add_post_type_support('page', 'excerpt');
+add_post_type_support('post', 'excerpt');
 
 // Custom image size
 add_image_size('my-custom-image-size', 1200);
@@ -30,10 +31,9 @@ add_image_size('thumbnail-grid', 400, 250);
 function my_custom_theme_enqueue()
 {
     wp_enqueue_style('typekit', 'https://use.typekit.net/liw6wlj.css');
-    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css');
     wp_enqueue_style('my-custom-theme', get_stylesheet_uri());
 
-    
+
     wp_enqueue_script('popper', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js');
     wp_enqueue_script('bootstrap-jp', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js');
 
@@ -46,6 +46,7 @@ function my_custom_theme_enqueue()
 
     }
 }
+
 add_action('wp_enqueue_scripts', 'my_custom_theme_enqueue');
 
 // Handle title tags
@@ -59,22 +60,23 @@ include 'custom-shortcodes.php';
 
 
 // Disable removal of p tags
-remove_filter( 'the_content', 'shortcode_unautop' );
+remove_filter('the_content', 'shortcode_unautop');
 
-remove_filter ('the_content', 'wpautop');
+remove_filter('the_content', 'wpautop');
 
-function prevent_deleting_pTags($init){
+function prevent_deleting_pTags($init)
+{
     $init['wpautop'] = false;
 
     return $init;
 }
 
 // Enable shortcodes
-add_filter( 'widget_text', 'shortcode_unautop');
-add_filter( 'widget_text', 'do_shortcode');
-add_filter( 'the_excerpt', 'shortcode_unautop');
-add_filter( 'the_excerpt', 'do_shortcode');
-add_filter( 'term_description', 'shortcode_unautop');
-add_filter( 'term_description', 'do_shortcode' );
-add_filter( 'comment_text', 'shortcode_unautop');
-add_filter( 'comment_text', 'do_shortcode' );
+add_filter('widget_text', 'shortcode_unautop');
+add_filter('widget_text', 'do_shortcode');
+add_filter('the_excerpt', 'shortcode_unautop');
+add_filter('the_excerpt', 'do_shortcode');
+add_filter('term_description', 'shortcode_unautop');
+add_filter('term_description', 'do_shortcode');
+add_filter('comment_text', 'shortcode_unautop');
+add_filter('comment_text', 'do_shortcode');

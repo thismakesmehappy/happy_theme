444aa57128d674db3aaba9766497f900afe185cf
Bernardo Margulis <bernardo@thismakesmehappy.co>
2023-09-28 09:53:22 -0400

    Refactor JS

--- footer.php
+++ footer.php
@@ -18,52 +18,12 @@
 <?php wp_footer(); ?>
 
 </body>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/secondaryNav.js" type="text/javascript">
+</script>
+
 <script>
-
-    const secondaryNav = document.getElementById("secondary-nav");
-    const topHeader = document.getElementById("top-header");
-
-    const isItemInView = (element) => {
-        const elementBottom = element.getBoundingClientRect().bottom;
-        const elementTop = element.getBoundingClientRect().top;
-        const windowSize = window.innerHeight;
-        return elementBottom >= 0 && elementTop <= windowSize;
-    };
-    const displayScrollElement = (element) => {
-        element.classList.add("scrolled");
-    }
-
-    const hideScrollElement = (element) => {
-        element.classList.remove("scrolled");
-    }
-
-    const hideWhenTriggerInView = (triggerElement, animateElement) => {
-        if (!isItemInView(topHeader)) {
-            displayScrollElement(secondaryNav);
-        } else {
-            hideScrollElement(secondaryNav);
-        }
-    }
-
-    const watchScroll = () => {
-        window.addEventListener('scroll', () => {
-            hideWhenTriggerInView(topHeader, secondaryNav);
-        })
-    };
-
     watchScroll();
-
-    const verticalStacking = () => {
-        const verticalStack = document.querySelectorAll(".stack-vertical a");
-        verticalStack.forEach(item => {
-            const inner = item.innerHTML;
-            const newInner = "<div>" + inner.split("").join("</br>") + "</div>";
-            item.innerHTML = newInner;
-        });
-    }
-
     verticalStacking();
-
 </script>
 
 
---
+++ js/secondaryNav.js
@@ -0,0 +1,39 @@
+const secondaryNav = document.getElementById("secondary-nav");
+const topHeader = document.getElementById("top-header");
+
+const isItemInView = (element) => {
+    const elementBottom = element.getBoundingClientRect().bottom;
+    const elementTop = element.getBoundingClientRect().top;
+    const windowSize = window.innerHeight;
+    return elementBottom >= 0 && elementTop <= windowSize;
+};
+const displayScrollElement = (element) => {
+    element.classList.add("scrolled");
+}
+
+const hideScrollElement = (element) => {
+    element.classList.remove("scrolled");
+}
+
+const hideWhenTriggerInView = (triggerElement, animateElement) => {
+    if (!isItemInView(topHeader)) {
+        displayScrollElement(secondaryNav);
+    } else {
+        hideScrollElement(secondaryNav);
+    }
+}
+
+const watchScroll = () => {
+    window.addEventListener('scroll', () => {
+        hideWhenTriggerInView(topHeader, secondaryNav);
+    })
+};
+
+const verticalStacking = () => {
+    const verticalStack = document.querySelectorAll(".stack-vertical a");
+    verticalStack.forEach(item => {
+        const inner = item.innerHTML;
+        const newInner = "<div>" + inner.split("").join("</br>") + "</div>";
+        item.innerHTML = newInner;
+    });
+}
